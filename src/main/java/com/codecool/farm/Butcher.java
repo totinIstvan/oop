6d78161f1509d10cceb56560242d7e8629f8c2d5
totinIstvan <totin.istvan@gmail.com>
2021-03-24 15:42:28 +0100

    Complete tasks

--- src/main/java/com/codecool/farm/Butcher.java
+++ src/main/java/com/codecool/farm/Butcher.java
@@ -1,5 +1,11 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+
 class Butcher {
+
+    boolean canButcher(Animal animal) {
+        return animal.getSize() > 4;
+    }
 
 }
--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,44 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
 
+    List<Animal> animals;
+
+    public Farm(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void feedAnimals() {
+        for (Animal animal : animals) {
+            animal.feed();
+        }
+    }
+
+    public void butcher(Butcher butcher) {
+        animals.removeIf(butcher::canButcher);
+    }
+
+    public boolean isEmpty() {
+        return animals.isEmpty();
+    }
+
+    public List<String> getStatus() {
+        String format = "There is a %d sized %s in the farm.";
+        List<String> statusesOfAnimals = new ArrayList<>();
+
+        for (Animal animal : animals) {
+            statusesOfAnimals.add(String.format(format, animal.getSize(), animal.getClass()
+                    .getSimpleName().toLowerCase()));
+        }
+        return statusesOfAnimals;
+    }
 }
